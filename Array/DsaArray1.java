5e97afa70ff9c2c773dfdc9e269fac466af32f7d
Param <ps8425@srmist.edu.in>
2023-09-18 15:31:59 +0530

    first commit

---
+++ Array/DsaArray1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+class DsaArray1{
+public static void main(String args[]){
+Scanner num=new Scanner(System.in);
+System.out.println("Enter the size of array - ");
+int size=num.nextInt();
+Scanner in=new Scanner(System.in);
+int arr[]=new int[size];
+for(int i=0;i<arr.length;i++){
+arr[i]=in.nextInt();
+}
+for(int ele:arr){
+System.out.print(ele+ " ");
+}
+}
+}
